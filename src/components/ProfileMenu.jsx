50d5457121d387020d0a411f5a1e10b36b548ac6
Antoniya-Aa <tonii.asenova@gmail.com>
2024-02-12 20:56:53 +0200

    Modify the Profile menu

--- src/components/ProfileMenu.jsx
+++ src/components/ProfileMenu.jsx
@@ -1,43 +1,80 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import LogoutButton from './LogoutButton';
+import { Link} from 'react-router-dom';
+import { logoutUser } from "../services/auth.service";
+import { AppContext } from "../context/AppContext";
+import { useContext } from "react";
+import Button from "./Button";
 
 export default function ProfileMenu() {
     const [isProfileMenuOpen, setProfileMenuOpen] = useState(false);
+    const { user, userData, setContext } = useContext(AppContext);
 
     const toggleProfileMenu = () => {
         setProfileMenuOpen(!isProfileMenuOpen);
     };
 
-    return (
-    <li
-        id="profile-menu"
-        className="nav-item"
-        onMouseEnter={toggleProfileMenu}
-        onMouseLeave={toggleProfileMenu}
-      >
-        <span className="nav-link">Профил</span>
-        {isProfileMenuOpen && (
-          <div id="dropdown-menu" className="dropdown-menu">
-            {/* Add your profile links here */}
-            <div className="profile-column">
-              <Link to="/favorites" className="profile-link">
-                Любими
-              </Link>
-              <Link to="/my-posts" className="profile-link">
-                Моите публикации
-              </Link>
-              <Link to="/my-comments" className="profile-link">
-                Моите коментари
-              </Link>
-              <Link to="/profile" className="profile-link">
-                Профил
-              </Link>
-              <LogoutButton />
-            </div>
-          </div>
-        )}
-      </li>
-    )
+    const logout = async () => {
+      await logoutUser();
+      setContext({ user: null, userData: null });
+    };
+
+    if(user){
+      return (
+        <li
+            id="profile-menu"
+            className="nav-item"
+            onMouseEnter={toggleProfileMenu}
+            onMouseLeave={toggleProfileMenu}
+          >
+            <span className="nav-link">Профил</span>
+            {isProfileMenuOpen && (
+              <div id="dropdown-menu" className="dropdown-menu">
+                {/* Add your profile links here */}
+                <div className="profile-column">
+                  <Link to="/favorites" className="profile-link">
+                    Любими
+                  </Link>
+                  <Link to="/my-posts" className="profile-link">
+                    Моите публикации
+                  </Link>
+                  <Link to="/my-comments" className="profile-link">
+                    Моите коментари
+                  </Link>
+                  <Link to="/profile" className="profile-link">
+                    Профил
+                  </Link>
+                  <Button onClick={logout}> Изход </Button>
+                </div>
+              </div>
+            )}
+          </li>
+        )
+    }else {
+      return (
+        <li
+            id="profile-menu"
+            className="nav-item"
+            onMouseEnter={toggleProfileMenu}
+            onMouseLeave={toggleProfileMenu}
+          >
+            <span className="nav-link">Профил</span>
+            {isProfileMenuOpen && (
+              <div id="dropdown-menu" className="dropdown-menu">
+                {/* Add your profile links here */}
+                <div className="profile-column">
+                <Link to="/login" className="profile-link">
+                    Вход
+                  </Link>
+                  <Link to="/register" className="profile-link">
+                    Регистрация
+                  </Link>
+                </div>
+              </div>
+            )}
+          </li>
+        )
+    }
+
+    
 
 }
